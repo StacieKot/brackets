12ee4ed31fe842693f0dbf28f86f7f522127ccc2
StacieKot <stacie.kot@gmail.com>
2021-03-05 19:07:27 +0300

    feat: add check function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   if(str.length%2!==0){
+      return false;
+   }
+   let bracketsConfigNew = [];
+   for(let i = 0; i<bracketsConfig.length; i++){
+    bracketsConfigNew[i] = bracketsConfig[i].join('');
+   }
+   let strLength = str.length;
+   for(let n = 0; n < strLength/2; n++){
+      for(let i = 0; i < bracketsConfigNew.length; i++){
+         str = str.split(bracketsConfigNew[i]).join('');
+      }
+   }
+   
+   return (str==='');
+ }
+
+
